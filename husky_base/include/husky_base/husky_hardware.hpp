4b15538731cad6a90e9320dd09e455a7f0158fe7
Georg Jäger <georg.jaeger@informatik.tu-freiberg.de>
2025-01-20 09:50:07 +0100

    Remove dependency on "hardware_interface/visibility_control.hpp"

--- husky_base/include/husky_base/husky_hardware.hpp
+++ husky_base/include/husky_base/husky_hardware.hpp
@@ -9,7 +9,6 @@
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "hardware_interface/visibility_control.h"
 #include "rclcpp/macros.hpp"
 
 #include <chrono>
@@ -28,24 +27,18 @@ class HuskyHardware : public hardware_interface::SystemInterface
 public:
   RCLCPP_SHARED_PTR_DEFINITIONS(HuskyHardware)
 
-  HARDWARE_INTERFACE_PUBLIC
   hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
-  HARDWARE_INTERFACE_PUBLIC
+  
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-
-  HARDWARE_INTERFACE_PUBLIC
+  
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-
-  HARDWARE_INTERFACE_PUBLIC
+  
   hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
-
-  HARDWARE_INTERFACE_PUBLIC
+  
   hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
-
-  HARDWARE_INTERFACE_PUBLIC
+  
   hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  HARDWARE_INTERFACE_PUBLIC
   hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
